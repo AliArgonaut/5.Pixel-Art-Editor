43a82936cfabeb40b9553cea4567f7d738ffafce
Lloyd <Lloydabonds@gmail.com>
2024-08-12 18:08:39 -0400

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const gridContainer = document.getElementById('gridContainer');
+
+
+const gridPixel = "<div class = 'gridPixel'> </div>"
+
+const gridRows = '50px '.repeat(600).trim(); 
+const gridColumns = '50px '.repeat(139).trim();
+
+gridContainer.style.gridTemplateRows = gridRows; 
+
+gridContainer.style.gridTemplateColumns = gridColumns;
+
+function createGrid(gridNum) {
+    for (let i = 0; i < gridNum; i++) {
+        gridContainer.innerHTML += gridPixel; 
+    } 
+}
+
+createGrid(16);
